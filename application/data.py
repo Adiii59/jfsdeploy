440baf783e71bfca251a259ffd5dc9ce3922325a
Adiii59 <65038718+Adiii59@users.noreply.github.com>
2023-07-28 05:59:42 +0530

    Add files via upload

---
+++ application/data.py
@@ -0,0 +1,67 @@
+MEMBERS = {
+    "shubh": {
+        "name": "Shubh Pathak",
+        "role": "Machine Learning Engineer",
+        "email": "shubhpathakpro@gmail.com",
+        "portfolio": "https://github.com/shubh0614",
+        "phone": "+91 95699 18724",
+        "img_url": "https://avatars.githubusercontent.com/u/75912395?v=4",
+        "projects": [("Project 1", "This is a project"), ("Project 2", "This is also a project")],
+        "experience": [("Experience 1", "This is an experience"), ("Experience 2", "This is also an experience")],
+        "linkedin": "https://www.linkedin.com/in/shubhpathak0614/",
+        "instagram": "#",
+        "github": "#"
+    }, 
+    "vishal": {
+        "name": "Vishal Srivastava",
+        "role": "UI/UX Designer, Web Developer",
+        "email": "vishalsrivastava8203@gmail.com",
+        "portfolio": "https://github.com/Dark8203",
+        "phone": "++91 83030 51506",
+        "img_url": "https://media.licdn.com/dms/image/C4D03AQFYRvM3XJGMLw/profile-displayphoto-shrink_800_800/0/1639591826143?e=2147483647&v=beta&t=YWTL9mVLOMOTZXTGryKMAzHna_r-p_DVL8hopQwa7PY",
+        "projects": [("Project 1", "This is a project"), ("Project 2", "This is also a project")],
+        "experience": [("Experience 1", "This is an experience"), ("Experience 2", "This is also an experience")],
+        "linkedin": "https://www.linkedin.com/in/vishal-srivastava-ceres/",
+        "instagram": "#",
+        "github": "#"
+    }, 
+    "ayushi": {
+        "name": "Ayushi Indu",
+        "role": "Web Developer",
+        "email": "singhayushi535@gmail.com",
+        "portfolio": "https://ayushi200116.github.io/",
+        "phone": "+91 93059 84620",
+        "img_url": "https://avatars.githubusercontent.com/u/71700207?v=4",
+        "projects": [("Project 1", "This is a project"), ("Project 2", "This is also a project")],
+        "experience": [("Experience 1", "This is an experience"), ("Experience 2", "This is also an experience")],
+        "linkedin": "https://www.linkedin.com/in/ayushi-indu-10147a1b6/",
+        "instagram": "#",
+        "github": "#"
+    }, 
+    "urvashi": {
+        "name": "Urvashi Indu",
+        "role": "Web Developer",
+        "email": "urvashiindu09@gmail.com",
+        "portfolio": "https://urvashi16.github.io/",
+        "phone": "+91 83032 63117",
+        "img_url": "https://avatars.githubusercontent.com/u/71694263?v=4",
+        "projects": [("Project 1", "This is a project"), ("Project 2", "This is also a project")],
+        "experience": [("Experience 1", "This is an experience"), ("Experience 2", "This is also an experience")],
+        "linkedin": "https://www.linkedin.com/in/urvashi-indu-a364791b6/",
+        "instagram": "#",
+        "github": "#"
+    }, 
+    "aditya": {
+        "name": "Aditya R",
+        "role": "Machine Learning Engineer",
+        "email": "adiramachandran59@gmail.com",
+        "portfolio": "https://adityarags.github.io/",
+        "phone": "+91 94446 82100",
+        "img_url": "https://adityarags.github.io/assets/images/my-avatar.png",
+        "projects": [("Project 1", "This is a project"), ("Project 2", "This is also a project")],
+        "experience": [("Experience 1", "This is an experience"), ("Experience 2", "This is also an experience")],
+        "linkedin": "https://www.linkedin.com/in/adityarags/",
+        "instagram": "#",
+        "github": "#"
+    }
+}
